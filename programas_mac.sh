601579d0886b2d609d008767e220ae48d3d78f3c
Eduardo Júnio <eduardojvr10@gmail.com>
2018-04-22 00:17:58 -0300

    Adicionando script de instalação de dependências e programas

---
+++ programas_mac.sh
@@ -0,0 +1,25 @@
+echo "============= Dependencias e programas MAC ============= "
+
+echo "--> Step 1....."
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+
+echo "--> Step 2....."
+curl http://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+echo "--> Step 3....."
+pip install virtualenv
+echo "Instrução: cd my_app --> virtualenv venv --> source venv/bin/activate"
+
+echo "--> Step 4...."
+brew install heroku/brew/heroku
+
+echo "--> Step 5....."
+brew install npm
+
+echo "--> Step 6....."
+cd
+git clone https://github.com/Eduardojvr/Personalizacao_Bash_MacBook.git
+cd Personalizacao_Bash_MacBook
+mv .bash_profile  ../
+
+echo "Fim"
